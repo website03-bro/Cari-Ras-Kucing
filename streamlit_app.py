bf218e6185e0bd7d51732ba23680ffe51222d8c7
website03-bro <linuxanto12@gmail.com>
2025-04-26 16:42:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ st.markdown(
 
 # Upload gambar
 st.markdown("---")
-uploaded_file = st.file_uploader("📤 Pilih gambar kucing...", type=["jpg", "jpeg", "png", "webp"])
+uploaded_file = st.file_uploader("📤 Pilih gambar kucing...", type=["jpg", "jpeg", "png", "webp","heif"])
 
 if uploaded_file:
     image = Image.open(uploaded_file)
